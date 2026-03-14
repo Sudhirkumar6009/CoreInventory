ae47a8ea426e445accc2957a2cf17dd2696077b8
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-03-14 16:58:33 +0530

    Delivery option fixing

--- client/src/pages/deliveries/DeliveryFormPage.jsx
+++ client/src/pages/deliveries/DeliveryFormPage.jsx
@@ -6,6 +6,7 @@ import { deliveryService } from "../../api/deliveryService";
 import { useDocumentTitle } from "../../hooks/useDocumentTitle";
 import { useRole } from "../../hooks/useRole";
 import { previewRef } from "../../utils/generateReference";
+import { STATUS_OPTIONS } from "../../constants";
 import Button from "../../components/common/Button";
 import StatusStepper from "../../components/common/StatusStepper";
 import LineItemTable from "../../components/common/LineItemTable";
